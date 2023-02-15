c51519fe6157f69c9460c3f9984e1ac8dd70c36b
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-14 22:50:13 -0500

    responsive footer only missing Instagram picture portion

--- components/sections/Banner.tsx
+++ components/sections/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner({pageTitle, bannerImage}: {pageTitle:string, bann
       <>
         <section className="md:rounded-lg md:m-5 md:h-40 lg:h-60 h-28 border-black border shadow-md  relative bg-myRed/[0.44] place-content-center grid">
             <img src={bannerImage} className="grayscale h-full w-full object-cover object-center absolute mix-blend-overlay"/>
-            <h1 className="font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] lg:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-white text-3xl lg:text-5xl text-center uppercase">
+            <h1 className="font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-white text-3xl md:text-5xl text-center uppercase">
                 {pageTitle}
             </h1>
         </section>
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -3,29 +3,34 @@ import Link from "next/link";
 import { navLinks } from "@/public/utils/data";
 import CompactLogo from "../elements/logos/CompactLogo";
 import WebsiteLogo from "../elements/logos/WebsiteLogo";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faFacebook, faFacebookF, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Footer() {
     const quote = `“Yesterday’s the past, tomorrow’s the future, but today is a gift. That’s why its called the present.” - Bil Keane`;
-    const instagramURL = 'https://www.instagram.com/hbcustemer/';
+    const INSTAGRAM_URL = 'https://www.instagram.com/hbcustemer/';
+    const FACEBOOK_URL = 'https://www.facebook.com/profile.php?id=100077784492698';
+    const YOUTUBE_URL = 'https://www.youtube.com/@hbcustemer5734';
 
 
     return (
       <>
         <section>
-          <p className='font-quote text-center text-lg'>{quote}</p>
+          <p className='font-quote text-center text-xl m-5'>{quote}</p>
           <div className="bg-black pt-10">
-            <div id="followus" className="bg-cyan-700 h-20 grid relative place-content-center">
+            <div className="bg-cyan-700 h-20 grid relative place-content-center">
               <div id="collage of Instagram images" className='absolute'>
               </div>
-              <Link href={instagramURL} className='text-white text-center uppercase font-title font-bold'>Follow Us On Instagram</Link>
+              <Link href={INSTAGRAM_URL} className='text-white text-center uppercase font-title font-bold'>Follow Us On Instagram</Link>
             </div>
-            <div className="my-10">
-              <div className="bg-myRed h-100 w-50">
-                <Link href={instagramURL} className="text-white"><FontAwesomeIcon icon={faCircle} className="text-white text-4xl"/>My Name is Nina</Link>
-                <FontAwesomeIcon icon={['fab', 'instagram']} className="text-white text-3xl inline"/>
+            <div className="my-10 md:flex md:w-full">
+              <div className="flex text-center h-8 my-10 md:ml-10 place-content-center">
+                <Link href={INSTAGRAM_URL}><FontAwesomeIcon icon={faInstagram} className="text-white h-full mx-1" mask={faCircle} transform="shrink-4" /></Link>
+                <Link href={FACEBOOK_URL}><FontAwesomeIcon icon={faFacebookF} className="text-white h-full mx-1" mask={faCircle} transform="shrink-6" /></Link>
+                <Link href={YOUTUBE_URL}><FontAwesomeIcon icon={faYoutube} className="text-white h-full mx-1" mask={faCircle} transform="shrink-6" /></Link>
               </div>
-              <div className="flex place-content-between mx-10">
+              <div className="flex place-content-between mx-10 md:mt-auto md:w-full">
                 <div>
                   <ul className="text-white font-body text-sm">
                     {navLinks.map((link, index) => { return(
@@ -33,13 +38,13 @@ export default function Footer() {
                     )})}
                   </ul>
                 </div>
-                <div className='self-end'>
+                <div className='self-end md:self-center'>
                   <CompactLogo color='#ffffff' className='block md:hidden h-20'/>
-                  <WebsiteLogo color='#ffffff' className="hidden md:block"/>
+                  <WebsiteLogo color='#ffffff' className="hidden md:block h-8"/>
                 </div>
               </div>
             </div>
-            <p className="font-body text-xs text-center text-white py-2">Designed and Build with ❤️ by Nina Fuentes LLC</p>
+            <p className="font-body text-xs md:text-sm text-center text-white py-2">Designed and Build with ❤️ by Nina Fuentes LLC</p>
           </div>
         </section>
       </>
