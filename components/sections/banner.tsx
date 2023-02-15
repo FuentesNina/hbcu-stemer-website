139ca0673973190fdeff48bd1e0c7e258883aede
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-15 15:29:23 -0500

    removed grayscale class on image. WARNING: styling is not correct

--- components/sections/banner.tsx
+++ components/sections/banner.tsx
@@ -6,13 +6,11 @@ export default function Banner() {
   const pageTitle = pageInfo.pageTitle;
   const bannerImage= pageInfo.bannerImage;
 
-  const overlay = 'bg-myRed/[0.44]'
-
     return (
       <>
-        <section className={`${overlay} md:rounded-lg md:border border-x-0 border md:m-5 md:h-40 lg:h-60 h-28 border-black shadow-md relative place-content-center grid`}>
-            <img src={bannerImage} className="grayscale h-full w-full object-cover object-center absolute mix-blend-overlay"/>
-            <h1 className="text-myGreen bg-myRed font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-3xl md:text-5xl text-center uppercase">
+        <section className={`bg-myRed/[0.44] md:rounded-lg md:border border-x-0 border md:m-5 md:h-40 lg:h-60 h-28 border-black shadow-md relative place-content-center grid`}>
+            <img src={bannerImage} className=" h-full w-full object-cover object-center absolute mix-blend-overlay"/>
+            <h1 className="text-white font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-3xl md:text-5xl text-center uppercase">
                 {pageTitle}
             </h1>
         </section>
