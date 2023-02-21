59b0cb914b532ec1e8b589544b3886e1088ae52e
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-21 16:27:43 -0500

    fixing deploy error

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <section className="max-w-lg min-h-fit m-auto overflow-clip">
         <CustomBackground backgroundColor="white" color="#d9d9d9" factor={2}>
-          <p className="bg-myRed/[0.5]">I'm seeing where this lands</p>
+          <p className="bg-myRed/[0.5]">{`I'm seeing where this lands`}</p>
         </CustomBackground>
       </section>
     </>
