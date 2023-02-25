a9dc36503cb257067e6385b18673a6801a2db5f6
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-24 19:22:58 -0500

    carousel is functional. only for 3 items

--- components/elements/carousel.tsx
+++ components/elements/carousel.tsx
@@ -1,12 +1,59 @@
 import { CommunityMemberCard } from "./communityMemberCard";
 import { communityMember } from "@/public/utils/data";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 export function Carousel() {
-    const member = communityMember.scholars[0];
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [animation, setAnimation] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            slide((currentSlide + 1) % 3);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [currentSlide])
+
+    const slide = function(index : number) {
+        if (currentSlide === 0 && index === 1) {
+            setAnimation('animate-halfLeft');
+        } else if (currentSlide === 1 && index === 2) {
+            setAnimation('animate-secondLeft');
+        } else if (currentSlide === 2 && index === 0) {
+            setAnimation('animate-fullLeft');
+        } else if (currentSlide === 2 && index === 1) {
+            setAnimation('animate-halfRight');
+        } else if (currentSlide === 1 && index === 0) {
+            setAnimation('animate-secondRight')
+        } else if (currentSlide === 0 && index === 2) {
+            setAnimation('animate-fullRight');
+        }
+        setCurrentSlide(index);
+    }
 
     return (
         <div className="pb-10">
-            <CommunityMemberCard member={member} />
+            <ul className={`flex mx-auto max-w-xs overflow-x-clip transition-transform justify-center`}>
+                {[...Array(3)].map((value, index) => {
+                    return (
+                        <li key={index} className={`mx-5 ${animation}`}>
+                            <CommunityMemberCard member={communityMember.scholars[index]} />
+                        </li>
+                    )
+                })}
+            </ul>
+            <ul className="flex justify-between w-24 mx-auto mt-5">
+                {[...Array(3)].map((value, index) => {
+                    return (
+                        <li key={index} className="relative h-4 w-4" onClick={() => slide(index)}>
+                            <FontAwesomeIcon icon={faCircle} className="absolute h-full" />
+                            <FontAwesomeIcon icon={faCircle} className={`${index !== currentSlide && 'hidden'} absolute text-myGreen h-full`} transform="shrink-3" />
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
