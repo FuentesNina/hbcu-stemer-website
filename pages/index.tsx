2757cdf319d482f39b8f0c18167254af9e13e384
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-25 12:03:46 -0500

    basic section done for medium screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,12 @@ import { Title } from "@/components/elements/title";
 import Link from "next/link";
 import { partnersData } from "@/public/utils/data";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import Countdown from "@/components/elements/countdown";
 import CustomBackground from "@/components/elements/customBackground";
 import { Carousel } from "@/components/elements/carousel";
-import { SectionTitle } from "@/components/elements/sectionTitle";
 
 export default function Home() {
-  const divStyle = "isolate bg-myRed/[0.44] overflow-clip rounded-3xl border border m-5 border-black shadow-md relative place-content-center grid py-5";
+  const divStyle = "isolate bg-myRed/[0.44] overflow-clip rounded-3xl border border m-5 border-black shadow-md relative place-content-center grid py-5 md:h-full md:m-0";
   const imageStyle = "grayscale mix-blend-overlay h-full w-full object-cover object-center absolute contrast-125 brightness-50"
   const h4Style = "font-display text-xl text-center font-bold uppercase text-white drop-shadow-[2px_2px_0_rgba(0,0,0,1)]";
   const pStyle = "drop-shadow-md font-body text-center text-white capitalize mt-2";
@@ -19,7 +17,7 @@ export default function Home() {
   return (
     <>
       <section id="hero" className="bg-black">
-        <div className="md:grid md:grid-cols-[3fr_2fr] md:pt-20 md:max-w-3xl md:mx-auto md:gap-10 md:px-5">
+        <div className="md:grid md:grid-cols-[3fr_2fr] md:pt-20 md:max-w-4xl md:mx-auto md:gap-10 md:px-5">
           <div className="max-w-xs mx-auto md:w-full">
             <LargeLogo color="white"  className="py-10 md:pt-0"/>
             <p className="capitalize font-body text-white text-center text-xl">{`empower potential`}</p>
@@ -27,11 +25,11 @@ export default function Home() {
               <Button content="learn more" buttonStyle="red" className="my-10 md:pb-0"/>
             </Link>
           </div>
-          <div className="aspect-video max-w-md mx-auto md:w-full md:my-auto md:order-first">
+          <div className="aspect-video max-w-md mx-auto md:w-full md:my-auto md:order-first md:shadow-white md:shadow-lg">
             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/o5caqBTLK4k?controls=0&autoplay=1&mute=1" title="HBCU STEMER Website Intro Video" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={false} />
           </div>
         </div>
-        <div className="py-10">
+        <div className="py-10 md:max-w-4xl md:mx-auto">
           <Title content="sponsored by" className="text-white text-sm text-center"/>
           <ul className="flex gap-10 justify-center mt-10 flex-wrap">
             {partnersData[0].companies.map((company, index) => {
@@ -44,7 +42,7 @@ export default function Home() {
           </ul>
         </div>
       </section>
-      <section id="basicInfo" className="scroll-mt-14 md:scroll-mt-24 my-10 max-w-md mx-auto">
+      <section id="basicInfo" className="scroll-mt-14 md:scroll-mt-24 my-10 max-w-md mx-auto md:grid md:grid-cols-2 md:max-w-4xl md:gap-5 md:my-20">
         <div>
           <Link href="/raceinfo" className="h-screen w-screen bg-red-100">
             <div className={`${divStyle} grid`}>
@@ -57,7 +55,7 @@ export default function Home() {
             </div>
           </Link>
         </div>
-        <div>
+        <div className="md:grid md:grid-rows-2 md:gap-5">
           <Link href="/">
             <div className={divStyle}>
               <Image className={imageStyle} fill src="/images/pledgeImage.png" alt=""/>
@@ -66,7 +64,7 @@ export default function Home() {
             </div>
           </Link>
           <Link href="/partners">
-          <div className={divStyle}>
+            <div className={divStyle}>
               <Image className={imageStyle} fill src="/images/ourPartners_Banner.jpg" alt=""/>
               <h4 className={h4Style}>{`sponsor`}</h4>
               <p className={pStyle}>{`Support STEM Education`}</p>
