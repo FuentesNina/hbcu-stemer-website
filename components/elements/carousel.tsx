ddc9ced94b9685b99c8e4cd05bcd2af3ba82084f
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-28 11:36:19 -0500

    attempt to fix overflow-clip happening on y-axis on mobile

--- components/elements/carousel.tsx
+++ components/elements/carousel.tsx
@@ -35,8 +35,8 @@ export function Carousel() {
     }
 
     return (
-        <div className="pb-10 md:order-first">
-            <ul className={`flex mx-auto max-w-sm overflow-x-clip transition-transform justify-center`}>
+        <div className="pb-10 md:order-first overflow-x-clip">
+            <ul className={`flex mx-auto max-w-sm transition-transform justify-center`}>
                 {[...Array(3)].map((value, index) => {
                     return (
                         <li key={index} className={`mx-5 ${animation} ${currentSlide !== index && ""}`}>
