5671404c8e7e4d67f5e643d497dc5604a554855e
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-28 11:39:28 -0500

    attempt to fix focused member displayed on mobile

--- components/elements/communityMemberCard.tsx
+++ components/elements/communityMemberCard.tsx
@@ -11,11 +11,11 @@ export function CommunityMemberCard({member, focusable}:{member: any, focusable?
 
     return (
         <>
-        <div className={`${(active && focusable) ? 'z-20 fixed left-0 right-0 top-[15vh] md:top-[40vh]' : 'hidden'} mx-auto w-56 h-fit grid justify-items-center`} onClick={() => setActive(true)}>
+        <div className={`${(active && focusable) ? 'z-20 fixed left-0 right-0 top-[15vh] md:top-[40vh]' : 'hidden'} mx-auto w-56 h-fit flex flex-col place-items-center`} onClick={() => setActive(true)}>
             <div className="aspect-square border border-black w-56 relative -mb-10" style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}}>
                 <img src={member.picture} className="object-cover h-full"/>
             </div>
-            <div className="bg-white p-5 pt-16 w-[150%] border border-black" style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}}>
+            <div className="bg-white p-5 pt-16 w-[150%] border border-black h-fit" style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}}>
                 <p className="font-title font-bold uppercase text-center text-sm">{`${member.firstName} ${member.lastName}`}</p>
                 <p className="font-title uppercase text-center text-sm">{member.school}</p>
                 <p className="font-title uppercase text-center text-sm">{member.event}</p>
