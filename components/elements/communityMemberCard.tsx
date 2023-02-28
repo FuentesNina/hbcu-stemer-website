cca57a55661094e84d0a4208ef736ba363af5c6c
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-02-27 19:16:38 -0500

    slight change in the focused card styling

--- components/elements/communityMemberCard.tsx
+++ components/elements/communityMemberCard.tsx
@@ -11,8 +11,8 @@ export function CommunityMemberCard({member, focusable}:{member: any, focusable?
 
     return (
         <>
-        <div className={`${(active && focusable) ? 'z-20 fixed left-0 right-0 top-[40vh]' : 'hidden'} mx-auto w-56 h-fit grid justify-items-center`} style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}} onClick={() => setActive(true)}>
-            <div className="aspect-square border border-black w-56 relative -mb-10">
+        <div className={`${(active && focusable) ? 'z-20 fixed left-0 right-0 top-[40vh]' : 'hidden'} mx-auto w-56 h-fit grid justify-items-center`} onClick={() => setActive(true)}>
+            <div className="aspect-square border border-black w-56 relative -mb-10" style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}}>
                 <img src={member.picture} className="object-cover h-full"/>
             </div>
             <div className="bg-white p-5 pt-16 w-[150%] border border-black" style={{boxShadow: '4px 4px 0 #ff1616, -4px -4px 0 #cfff00'}}>
