10814efa333a2ac31b081a672c4ce20ef87071ca
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-03-08 22:32:01 -0600

    impact game is functional and responsive

--- components/sections/impactGame.tsx
+++ components/sections/impactGame.tsx
@@ -1,54 +1,98 @@
 import { getPageInfo } from "@/hooks/getPageInfo";
-import Image from "next/image";
 import Button from "../elements/styledbutton";
 import { milestones } from "@/public/utils/data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft, faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 export default function ImpactGame() {
   const pageInfo = getPageInfo();
-
   const pageTitle = pageInfo.pageTitle;
 
-    return (
-      <section className={`pb-12 isolate bg-gradient-to-b from-myRed to-transparent overflow-clip md:rounded-lg md:border border-x-0 border md:m-5 border-black shadow-md relative place-content-center grid`}>
-          <div className="mt-5 mb-10">
-            <h1 className="mt-10 text-white font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-3xl md:text-5xl text-center uppercase">
-                {pageTitle}
-            </h1>
-          </div>
-          <div className="flex my-10">
-            <FontAwesomeIcon icon={faArrowAltCircleLeft} className="h-10 cursor-pointer mr-10"/>
-            <div>
-              <div className="hidden">
-                <p className="font-body font-bold text-center text-sm text-white">This is a text that invites people to scroll through the race</p>
-                <Button content='begin the race' href='' buttonStyle="black"/>
-                <Button content='meet our scholars' href='#community' buttonStyle="black" className="hidden"/>
-              </div>
-              <div className="border border-black my-5 max-w-xs grid divide-y divide-black shadow-[4px_4px_0_black] mx-auto">
-                <h3 className="bg-myRed text-white font-title uppercase font-bold px-5 py-3 text-sm">{milestones[0].title}</h3>
-                <p className="bg-white font-body p-5 text-sm">{milestones[0].description}</p>
-              </div>
-            </div>
-            <FontAwesomeIcon icon={faArrowAltCircleRight} className="h-10 cursor-pointer ml-10"/>
-          </div>
+  const [status, setStatus] = useState('start');
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const indexLimit = milestones.length;
 
-          <div className='flex justify-around items-baseline'>
-            <div>
-              <img src='/images/impact_scene/runningMan_logo.png' className="h-20 -z-10 hidden"/>
-              <img src='/images/impact_scene/startingMan_logo.png' className="h-20 -z-10"/>
-              <img src='/images/impact_scene/finishMan_logo.png' className="h-32 -z-10 hidden"/>
-            </div>
-            <div className="h-32">
-              <img src='/images/impact_scene/start_banner.png' className="h-full -z-10"/>
-              <img src='/images/impact_scene/finish_banner.png' className="h-20 -z-10 hidden"/>
-            </div>
-          </div>
-
-          <div className="h-full absolute w-full bg-[url('/images/impact_scene/background.png')] bg-cover bg-repeat-x -z-20" style={{backgroundPositionX: '0%', backgroundPositionY: '100%'}}/>
-          <div className="h-full absolute w-full bg-[url('/images/impact_scene/midground.png')] bg-cover bg-repeat-x -z-20" style={{backgroundPositionX: '0%', backgroundPositionY: '100%'}}/>
-          <div className="h-full absolute w-full bg-[url('/images/impact_scene/ground.png')] bg-cover bg-repeat-x -z-20" style={{backgroundPositionX: '0%', backgroundPositionY: '100%'}}/>
-          <div className="h-full absolute w-full bg-[url('/images/impact_scene/foreground.png')] bg-cover bg-repeat-x -z-10" style={{backgroundPositionX: '0%', backgroundPositionY: '100%'}}/>
-      </section>
-    )
+  const startRace = function() {
+    if (status === 'end') {
+      setStatus('start');
+      setCurrentSlide(0);
+    } else {
+      setStatus('run');
+      setCurrentSlide(0);
+    }
   }
+
+  const changeSlide = function(direction : number) {
+    let nextIndex = currentSlide + direction;
+
+    if (status === 'start' && nextIndex === 1) {
+      setStatus('run');
+    } else if (status === 'end' && nextIndex === indexLimit - 2) {
+      setStatus('run');
+    } else if (nextIndex === -1) {
+      setStatus('start');
+    } else if (nextIndex === indexLimit) {
+      setStatus('end');
+    } else {
+      setCurrentSlide(nextIndex);
+    }
+  }
+
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    console.log(windowWidth)
+
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+
+    return window.removeEventListener('resize', () => setWindowWidth(window.innerWidth));
+  })
+
+  return (
+    <section className={`flex flex-col isolate bg-gradient-to-b from-myRed to-transparent overflow-clip md:rounded-lg md:border border-x-0 border md:m-5 border-black shadow-md relative`}>
+      <div className="my-10 mt-20 md:my-10">
+        <h1 className=" text-white font-display drop-shadow-[2px_2px_0_rgba(0,0,0,1)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,1)] font-bold text-3xl md:text-5xl text-center uppercase">
+            {pageTitle}
+        </h1>
+      </div>
+      <div className="md:my-5 md:mt-0 my-10">
+        <div className="h-52 grid place-content-center overflow-y-auto">
+          <div className={`${status === 'run' && 'hidden'}`}>
+            <p className={`${status !== 'start' && 'hidden'} mx-10 drop-shadow-md font-body font-bold text-center text-sm`}>This is a text that invites people to scroll through the race</p>
+            <Button className={`${status === 'start' && 'animate-pulse'} my-5`} action={startRace} content={status === 'start' ? 'begin the race' : 'start over'} href='' buttonStyle={status === 'start' ? 'black' : 'darkerGrey'}/>
+            <Button content='meet our scholars' href='#community' buttonStyle="black" className={`${status !== 'end' && 'hidden'}`}/>
+          </div>
+          <div className={`${status === 'run' ? 'grid' : 'hidden'} border border-black max-w-xs divide-y divide-black shadow-[4px_4px_0_black] mx-auto`}>
+            <h3 className="bg-myRed text-white font-title uppercase font-bold px-5 py-3 text-sm">{milestones[currentSlide].title}</h3>
+            <p className="bg-white font-body p-5 text-sm">{milestones[currentSlide].description}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between my-10 md:my-0 max-w-4xl md:mx-auto md:w-full">
+        <FontAwesomeIcon onClick={() => changeSlide(-1)} icon={faArrowAltCircleLeft} className={`${status !== 'start' && 'cursor-pointer'} ml-5 shrink-0 h-10 mr-10 text-white drop-shadow-xl`}/>
+        <FontAwesomeIcon onClick={() => changeSlide(1)} icon={faArrowAltCircleRight} className={`mr-5 shrink-0 h-10 ${status !== 'end' && 'cursor-pointer'} ml-10 text-white drop-shadow-xl`}/>
+      </div>
+
+      <div className='pointer-events-none select-none flex justify-around items-baseline mb-16 md:mb-12 md:max-w-4xl md:mx-auto md:w-full'>
+        <div>
+          <img src='/images/impact_scene/startingMan_logo.png' className={`${status !== 'start' && 'hidden'} h-20 -z-10 relative`}/>
+          <img src='/images/impact_scene/runningMan_logo.png' className={`${status !== 'run' && 'hidden'} h-20 -z-10 relative`}/>
+          <img src='/images/impact_scene/finishMan_logo.png' className={`${status !== 'end' && 'hidden'} h-32 -z-10 relative animate-bounce`}/>
+        </div>
+        <div className={`h-32 ${status === 'end' && 'order-first'}`}>
+          <img src='/images/impact_scene/start_banner.png' className={`${status !== 'start' && 'hidden'} h-full -z-10 relative`}/>
+          <img src='/images/impact_scene/finish_banner.png' className={`${status !== 'end' && 'hidden'} h-full -z-10 relative`}/>
+        </div>
+      </div>
+
+      <div className="h-full absolute w-full bg-[url('/images/impact_scene/background.png')] bg-cover lg:bg-contain bg-repeat-x -z-20 transition-all" style={{backgroundPositionX: `${(status === 'start' ? -1 : status === 'end' ? indexLimit : currentSlide) * (windowWidth < 1024 ? 10 : -25)}%`}}/>
+      <div className="h-full absolute w-full bg-[url('/images/impact_scene/midground.png')] bg-cover lg:bg-contain bg-repeat-x -z-20 transition-all" style={{backgroundPositionX: `${(status === 'start' ? -1 : status === 'end' ? indexLimit : currentSlide) * (windowWidth < 1024 ? 25 : -50)}%`}}/>
+      <div className="h-full absolute w-full bg-[url('/images/impact_scene/ground.png')] bg-cover lg:bg-contain bg-repeat-x -z-20 transition-all" style={{backgroundPositionX: `${(status === 'start' ? -1 : status === 'end' ? indexLimit : currentSlide) * (windowWidth < 1024 ? 50 : -100)}%`}}/>
+      <div className="h-full absolute w-full bg-[url('/images/impact_scene/foreground.png')] bg-cover lg:bg-contain bg-repeat-x -z-10 transition-all" style={{backgroundPositionX: `${(status === 'start' ? -1 : status === 'end' ? indexLimit : currentSlide) * (windowWidth < 1024 ? 50 : -100)}%`}}/>
+
+      <div className="bg-myGreen h-2 transition-all" style={{width: `${(status === 'start' ? 0 : (currentSlide + 1))/(indexLimit) * 100}%`}}/>
+    </section>
+  )
+}
--- pages/impact.tsx
+++ pages/impact.tsx
@@ -19,8 +19,7 @@ export default function Impact() {
   return (
     <>
       <ImpactGame />
-      {/* <Banner /> */}
-      <section className="md:my-20" id='community'>
+      <section className="md:my-20 scroll-mt-10 md:scroll-mt-20" id='community'>
         <div className="my-10 grid gap-10 mx-5 md:my-20">
           <SectionTitle content={`our community`} titleStyle="red-black"/>
           <p className="font-body text-justify md:px-10 md:max-w-5xl md:mx-auto">{`This is a small intro paragraph talking about the people who are impacted by HBCU STEMER`}</p>
