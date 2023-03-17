62dd28c1288ca1aa954c9e8edec53e5d77dd4627
Nina Fuentes <nina.fuentes.n@gmail.com>
2023-03-17 14:56:56 -0400

    first attempt at data setup

---
+++ .tina/config.ts
@@ -0,0 +1,80 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "b774f2c1-4ef4-49b4-a532-771e076cc3ae", // Get this from tina.io
+  token: "6c4bbe8434a215c4aa84bccedd0ac30dbf075b84", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+      {
+        name: "webLinks",
+        label: "WebLinks",
+        path: "content/webLinks",
+        fields: [
+          {
+            type: "string",
+            name: "instagram",
+            label: "Instagram",
+          },
+          {
+            type: "string",
+            name: "facebook",
+            label: "Facebook",
+          },
+          {
+            type: "string",
+            name: "youtube",
+            label: "YouTube",
+          },
+          {
+            type: "string",
+            name: "email",
+            label: "Contact Email",
+          },
+          {
+            type: "string",
+            name: "quote",
+            label: "Quote",
+          },
+        ],
+      },
+    ],
+  },
+});
--- components/sharedSections/footer.tsx
+++ components/sharedSections/footer.tsx
@@ -5,34 +5,56 @@ import WebsiteLogo from "../elements/logos/websiteLogo";
 import InstagramShowcase from "../elements/instagramShowcase";
 import SocialMediaIcons from "../elements/socialMediaIcons";
 import NewsletterForm from "../sections/newsletterForm";
+import { useTina } from "tinacms/dist/react";
+import client from "@/.tina/__generated__/client";
 
 
-export default function Footer({showSignUp}: {showSignUp: boolean}) {
+export default function Footer({showSignUp, props}: {showSignUp: boolean, props:any}) {
+  const { data } = useTina ({
+    query: props.query,
+    variables: props.variables,
+    data: props.variables,
+  })
 
-    return (
-      <footer className="overflow-clip w-full">
-        <p className={`font-quote text-center text-lg mx-5 ${showSignUp ? 'relative top-10 mb-5' : 'my-5'}`}>{webLinks.quote}</p>
-        {showSignUp && <NewsletterForm />}
-        <div className={`bg-black -z-10 ${showSignUp ? 'pt-20' : 'pt-10'}`}>
-          <InstagramShowcase customUrl={webLinks.instagram} height="112px"/>
-          <div className="my-10 md:flex md:w-full">
-            <SocialMediaIcons />
-            <div className="flex place-content-between mx-10 md:mt-auto md:w-full">
-              <nav>
-                <ul className="text-white font-body text-sm capitalize">
-                  {navLinks.map((pageInfo, index) => { return(
-                    <li key={index}><Link href={pageInfo.path} className="hover:font-bold hover:text-myGreen">{pageInfo.pageTitle}</Link></li>
-                  )})}
-                </ul>
-              </nav>
-              <Link href="/" className="self-end md:self-center">
-                <CompactLogo color='#ffffff' className='block md:hidden h-14'/>
-                <WebsiteLogo color='#ffffff' className="hidden md:block h-8"/>
-              </Link>
-            </div>
+  console.log(data);
+
+
+  return (
+    <footer className="overflow-clip w-full">
+      <p className={`font-quote text-center text-lg mx-5 ${showSignUp ? 'relative top-10 mb-5' : 'my-5'}`}>{webLinks.quote}</p>
+      {showSignUp && <NewsletterForm />}
+      <div className={`bg-black -z-10 ${showSignUp ? 'pt-20' : 'pt-10'}`}>
+        <InstagramShowcase customUrl={webLinks.instagram} height="112px"/>
+        <div className="my-10 md:flex md:w-full">
+          <SocialMediaIcons />
+          <div className="flex place-content-between mx-10 md:mt-auto md:w-full">
+            <nav>
+              <ul className="text-white font-body text-sm capitalize">
+                {navLinks.map((pageInfo, index) => { return(
+                  <li key={index}><Link href={pageInfo.path} className="hover:font-bold hover:text-myGreen">{pageInfo.pageTitle}</Link></li>
+                )})}
+              </ul>
+            </nav>
+            <Link href="/" className="self-end md:self-center">
+              <CompactLogo color='#ffffff' className='block md:hidden h-14'/>
+              <WebsiteLogo color='#ffffff' className="hidden md:block h-8"/>
+            </Link>
           </div>
-          <p className="font-body text-xs md:text-sm text-center text-white py-2">Designed and Build with ❤️ by <Link href="https://ninafuentes.dev" className="underline hover:font-bold">Nina Fuentes LLC</Link></p>
         </div>
-      </footer>
-    )
+        <p className="font-body text-xs md:text-sm text-center text-white py-2">Designed and Build with ❤️ by <Link href="https://ninafuentes.dev" className="underline hover:font-bold">Nina Fuentes LLC</Link></p>
+      </div>
+    </footer>
+  )
+}
+
+export const getStaticProps = async () => {
+  const { data, query, variables } = await client.queries.webLinks({relativePath: 'site-details.md'});
+
+  return {
+    props: {
+      data,
+      query,
+      variables,
+    }
   }
+};
